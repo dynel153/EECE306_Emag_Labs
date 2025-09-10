79ab73efec8be8ab84fbafb0938609c189f7a927
Dylan Canel <mcanel04@yahoo.com>
2025-09-10 08:32:43 -0400

    Added the notes taken from doing the practice question

--- Lab2/ML0202.m
+++ Lab2/ML0202.m
@@ -24,3 +24,12 @@ x = r*cos(theta); % calulate the x coordinate
 y = r*sin(theta); % calculate the y coordinate
 z = z;
 cart = [x, y, z]; % create the Cartesian coordinate point
+
+% for pol2cart must write (degree in radian, Radius, the Z)
+
+
+% Practice Problem 1; [x,y,z] = pol2cart (pi,5,2)
+% OutPut = x = -5 y = 6.1232e-16 z = 2 accurate to the script output 
+
+% Pracrice Problem 2: [theta, r,z]= cart2pol(3,5,9)
+% output; theta = 1.0304 r = 5.8310 z = 9
